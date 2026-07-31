ef16c1c8c3081fbd3355406b17430b226c7d4957
Richard117297 <richardcarvajalb@gmail.com>
2026-07-30 20:50:07 -0500

    chore: preparar despliegue de PixelPets en Render

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,7 +2,8 @@ import { app } from "./app.js";
 
 const configuredPort = Number(process.env.PORT);
 const port = Number.isInteger(configuredPort) && configuredPort > 0 ? configuredPort : 3000;
+const host = "0.0.0.0";
 
-app.listen(port, () => {
-  console.log(`PixelPets API disponible en http://localhost:${port}/api`);
+app.listen(port, host, () => {
+  console.log(`PixelPets disponible en http://${host}:${port}`);
 });
